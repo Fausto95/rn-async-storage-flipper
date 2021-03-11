f7cf20eba76787ff1a69fd6df31057b35893b04c
Francisco Morais <francisco.luis.morais@gmail.com>
2021-03-11 14:41:56 +0000

    Fix flipper bootstrap plugin handler

--- packages/rn-async-storage-flipper/index.js
+++ packages/rn-async-storage-flipper/index.js
@@ -17,26 +17,57 @@ function ReactNativeAsyncStorageFlipper(storage) {
     bootstrapPlugin()
         .then((currentConnection) => {
             if (currentConnection) {
-                storage.getAllKeys().then((keys) => {
-                    storage.multiGet(keys).then((data) => {
-                        data.map((_, i, store) => {
-                            let key = store[i][0];
-                            let value = store[i][1];
-                            let needsToBeParsed =
-                                (value.startsWith('{') &&
-                                    value.endsWith('}')) ||
-                                (value.startsWith('[') && value.endsWith(']'));
-                            if (needsToBeParsed) {
-                                value = JSON.parse(value);
-                            }
-                            currentConnection.send('newElement', {
-                                key,
-                                value,
-                                id: key,
-                            });
-                        });
-                    });
+              const setItem = storage.setItem;
+              const removeItem = storage.removeItem;
+
+              storage.setItem = (key, value, ...rest) => {
+                setItem(key, value, ...rest);
+
+                let sendValue = value;
+                let needsToBeParsed = (sendValue.startsWith('{') && sendValue.endsWith('}')) || (sendValue.startsWith('[') && sendValue.endsWith(']'));
+
+                if (needsToBeParsed) {
+                  sendValue = JSON.parse(sendValue);
+                }
+
+                currentConnection.send('newElement', {
+                  key,
+                  value: sendValue,
+                  id: key,
                 });
+              };
+
+              storage.removeItem = (key, ...rest) => {
+                removeItem(key, ...rest);
+                currentConnection.send('newElement', {
+                  key,
+                  value: null,
+                  id: key,
+                });
+              };
+
+              storage.getAllKeys().then((keys) => {
+                storage.multiGet(keys).then((data) => {
+                  data.map((_, i, store) => {
+                    let key = store[i][0];
+                    let value = store[i][1];
+                    let needsToBeParsed =
+                      (value.startsWith('{') &&
+                      value.endsWith('}')) ||
+                      (value.startsWith('[') && value.endsWith(']'));
+
+                    if (needsToBeParsed) {
+                      value = JSON.parse(value);
+                    }
+
+                    currentConnection.send('newElement', {
+                      key,
+                      value,
+                      id: key,
+                    });
+                  });
+                });
+              });
             }
         })
         .catch((err) => console.error(err));
